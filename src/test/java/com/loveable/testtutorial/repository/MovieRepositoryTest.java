2561d6a0a4a171f5f23e3e81b8bf483d348f538a
Mensaloveable <abdullahi.adedayo@gmail.com>
2023-01-13 08:33:02 +0100

    Movie Repository Test (movie database size)

--- src/test/java/com/loveable/testtutorial/repository/MovieRepositoryTest.java
+++ src/test/java/com/loveable/testtutorial/repository/MovieRepositoryTest.java
@@ -8,7 +8,10 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DataJpaTest
@@ -31,7 +34,32 @@ class MovieRepositoryTest {
 
         //Assert
         assertNotNull(newMovie);
-//        assertThat(newMovie.getId()).isNotEqualTo(null);
+        assertThat(newMovie.getId()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("It should return he movie list with size of 2")
+    void getAllSize() {
+        //Arrange
+        Movie avatar = new Movie();
+        avatar.setName("Avatar");
+        avatar.setGenre("Action");
+        avatar.setReleaseDate(LocalDate.of(2000, Month.APRIL, 22));
+
+        Movie titanic = new Movie();
+        avatar.setName("Titanic");
+        avatar.setGenre("Romance");
+        avatar.setReleaseDate(LocalDate.of(1999, Month.MAY, 22));
+
+        //Act
+        movieRepository.save(avatar);
+        movieRepository.save(titanic);
+        List<Movie> allMovies = movieRepository.findAll();
+
+        //Assert
+        assertNotNull(allMovies);
+        assertThat(allMovies).isNotNull();
+        assertEquals(2, allMovies.size());
     }
 
 }
