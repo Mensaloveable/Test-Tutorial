3285236bb3962047868b4e42a8c70fc18c075b66
Mensaloveable <abdullahi.adedayo@gmail.com>
2023-01-15 08:35:25 +0100

    Create MovieControllerTest class

---
+++ src/main/java/com/loveable/testtutorial/controllers/MovieController.java
@@ -0,0 +1,45 @@
+package com.loveable.testtutorial.controllers;
+
+import com.loveable.testtutorial.model.Movie;
+import com.loveable.testtutorial.services.MovieService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class MovieController {
+    private final MovieService movieService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Movie create(@RequestBody Movie movie) {
+        return movieService.save(movie);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Movie> getAllMovies() {
+        return movieService.getAllMovies();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Movie getMovie(@PathVariable Long id){
+        return movieService.getMovieById(id);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Movie updateMovie(@PathVariable Long id, @RequestBody Movie movie) {
+        return movieService.updateMovie(movie, id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteMovie(@PathVariable Long id) {
+        movieService.deleteMovie(id);
+    }
+}
---
+++ src/test/java/com/loveable/testtutorial/controllers/MovieControllerTest.java
@@ -0,0 +1,28 @@
+package com.loveable.testtutorial.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovieControllerTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void getAllMovies() {
+    }
+
+    @Test
+    void getMovie() {
+    }
+
+    @Test
+    void updateMovie() {
+    }
+
+    @Test
+    void deleteMovie() {
+    }
+}
